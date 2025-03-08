613d31a31cf09332195c5cdb81ee44f03cf1eb5a
Mg Htoo <whshwe@gmail.com>
2025-03-08 14:29:47 +0630

    cleanup interactionManager.js

--- src/js/components/interactionManager.js
+++ src/js/components/interactionManager.js
@@ -1,10 +1,10 @@
 export function interactionManager(globals) {
-  const { selectors } =  globals;
+  const { selectors } = globals;
   const { squareFrame } = selectors;
 
   // Object to manage the placed words data.
-  const placedWordData = {
-    _placedWordDetails : new Map(),
+  const _placedWordData = { // private
+    _placedWordDetails: new Map(), //private
 
     setWordDetails(selectedWord, currentWordSquareIDs) {
       this._placedWordDetails.set(selectedWord, currentWordSquareIDs);
@@ -14,7 +14,7 @@ export function interactionManager(globals) {
       return this._placedWordDetails.has(selectedWord);
     },
 
-    getWordDetails(selectedWord, type) { //new function.
+    getWordDetails(selectedWord, type) {
       const squareIDs = this._placedWordDetails.get(selectedWord);
       if (!squareIDs) return null; // return null instead of undefined.
       if (type === "start") return squareIDs[0];
@@ -30,92 +30,94 @@ export function interactionManager(globals) {
     getWordDetailsSize() {
       return this._placedWordDetails.size;
     },
-  }
+  };
 
   // Object to manage the endPoint state
-  const interactionState = {
-    endPointFlag: null,
+  const _interactionState = { //private
+    _endPointFlag: null, //private
 
     setEndPointFlag(value) {
-      this.endPointFlag = value;
+      this._endPointFlag = value;
     },
 
     getEndPointFlag() {
-      return this.endPointFlag;
+      return this._endPointFlag;
     },
-  }
-  
+  };
+
   // Store a map to hold all the listener functions.
-  const squareClickListeners = new Map();
+  const _squareClickListeners = new Map(); // private
 
   // Adds a click event listener to a specific square.
-  function addClickListener(placedWordDetails) {
+  function addClickListener(placedWordDetails) { //exposed function
     console.group("addClickListener()");
-    const { selectedWord, currentWordSquareIDs, } = placedWordDetails;
+    const { selectedWord, currentWordSquareIDs } = placedWordDetails;
 
-    placedWordData.setWordDetails(selectedWord, currentWordSquareIDs);
-    const startPoint = placedWordData.getWordDetails(selectedWord, "start");
-    const endPoint = placedWordData.getWordDetails(selectedWord, "end");
+    _placedWordData.setWordDetails(selectedWord, currentWordSquareIDs);
+    const startPoint = _placedWordData.getWordDetails(selectedWord, "start");
+    const endPoint = _placedWordData.getWordDetails(selectedWord, "end");
+
+    // create an object for the selectedWord, that is going to include all the listeners in it.
+    _squareClickListeners.set(selectedWord, {});
 
     const squareMap = new Map([
       ["start", startPoint],
       ["end", endPoint],
     ]);
-
-    // create an object for the selectedWord, that is going to include all the listeners in it.
-    squareClickListeners.set(selectedWord, {});
-
-    squareMap.forEach((squareID, type, squareMap) => {
+    squareMap.forEach((squareID, type) => {
       const square = document.querySelector(`#${squareID}`);
       if (square) {
-        const listener = () => { // later to be used with the removeEventListener
-          handleSquareClick(selectedWord, type, squareID, squareMap);
+        const listener = () => {
+          _handleSquareClick(selectedWord, type, squareID);
         };
         square.addEventListener("click", listener);
-        squareClickListeners.get(selectedWord)[squareID] = listener;
-        } else {
+        _squareClickListeners.get(selectedWord)[squareID] = listener;
+      } else {
         console.warn(`Square with ID ${squareID} not found.`);
       }
     });
     console.groupEnd();
   }
 
-  function handleStartSquareClick(interactionState, squareMap) {
-    interactionState.setEndPointFlag(squareMap.get("end"));
-    console.info("Start squareID Clicked, endPoint:",interactionState.getEndPointFlag());
+  function _handleStartSquareClick(selectedWord) { 
+    _interactionState.setEndPointFlag(_placedWordData.getWordDetails(selectedWord,"end"));
+    console.info(
+      "Start squareID Clicked, endPoint:",
+      _interactionState.getEndPointFlag()
+    );
   }
 
-  function handleEndSquareClick(placedWordData, interactionState, squareID, selectedWord, squareFrame) {
-    if (squareID === interactionState.getEndPointFlag()) { 
+  function _handleEndSquareClick(squareID, selectedWord) { 
+    if (squareID === _interactionState.getEndPointFlag()) {
       console.warn("BINGOOOOO!!!!");
+      const squaresToFill = _placedWordData.getWordDetails(selectedWord, "squareIDs");
+      _highlightCompletedWord(squaresToFill);
+      _markCompletedWord(selectedWord);
+      _handleGameCompletion(selectedWord);
+      _removeClickListener(selectedWord);
     }
   }
 
-  function handleSquareClick(selectedWord, type, squareID, squareMap) {
-    if (type === "start") { 
-      handleStartSquareClick(interactionState, squareMap);
+  function _handleSquareClick(selectedWord, type, squareID) { 
+    if (type === "start") {
+      _handleStartSquareClick(selectedWord);
     }
     if (type === "end") {
-      handleEndSquareClick(placedWordData, interactionState, squareID, selectedWord, squareFrame);
-      const squaresToFill = placedWordData.getWordDetails(selectedWord, "squareIDs");
-      highlightCompletedWord(squaresToFill);
-      markCompletedWord(selectedWord);
-      handleGameCompletion(placedWordData, selectedWord, squareFrame);
-      removeClickListener(selectedWord);
+      _handleEndSquareClick(squareID, selectedWord);
     }
   }
 
-  function handleGameCompletion(placedWordData, selectedWord, squareFrame) {
-    placedWordData.removeWordDetails(selectedWord);
-    if (placedWordData.getWordDetailsSize() === 0) {
+  function _handleGameCompletion(selectedWord) { 
+    _placedWordData.removeWordDetails(selectedWord);
+    if (_placedWordData.getWordDetailsSize() === 0) {
       squareFrame.classList.add("dim");
     }
   }
 
-  function removeClickListener(selectedWord) {
-    const listeners = squareClickListeners.get(selectedWord);
+  function _removeClickListener(selectedWord) { 
+    const listeners = _squareClickListeners.get(selectedWord);
     if (listeners) {
-      Object.keys(listeners).forEach(squareID => {
+      Object.keys(listeners).forEach((squareID) => {
         const square = document.querySelector(`#${squareID}`);
         if (square) {
           square.removeEventListener("click", listeners[squareID]);
@@ -124,24 +126,24 @@ export function interactionManager(globals) {
           listeners[squareID] = null; // good practice to remove the listener reference.
         }
       });
-      squareClickListeners.delete(selectedWord);
+      _squareClickListeners.delete(selectedWord);
     }
   }
 
   // Fill the squares with the highlight class.
-  function highlightCompletedWord(squares) {
-    squares.forEach(square => {
+  function _highlightCompletedWord(squares) { 
+    squares.forEach((square) => {
       const squareID = `#${square}`;
       document.querySelector(squareID).classList.add("highlight");
     });
   }
 
-  function markCompletedWord(id) {
+  function _markCompletedWord(id) { 
     const foundWordElement = document.querySelector(`#${id}`);
     foundWordElement.classList.add("dim", "marked");
   }
 
   return {
     addClickListener,
-  }
+  };
 }
