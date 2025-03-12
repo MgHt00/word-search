01cc1a37155441ea8e65741d2f50bc03c02cdc06
Mg Htoo <whshwe@gmail.com>
2025-03-12 16:52:33 +0630

    implementing scopeFinder

--- src/js/components/interactionManager.js
+++ src/js/components/interactionManager.js
@@ -1,3 +1,5 @@
+import { globals } from "../services/globals";
+
 export function interactionManager(globals) {
   const { appData } = globals; 
   const { selectors } = globals;
@@ -253,6 +255,7 @@ export function interactionManager(globals) {
   }
 
   function _handleSquareHover(squareDOMElement) {
+    //_scopeMgr._getSurroundingScrope(squareDOMElement.id);
     if (_interactionState.getTracerFlag()) {
       squareDOMElement.classList.add("tracer");
     }
@@ -267,4 +270,77 @@ export function interactionManager(globals) {
     addClickListener,
     addTracerListener,
   };
+}
+
+//const _scopeMgr = _scopeFinder();
+
+function _scopeFinder() {
+  const _hoverScope = new Set();
+
+  function extractNumbers(squareIdString) {
+    const regex = /^sq-(\d+)-(\d+)$/; // [le7]Regular expression to match the pattern 
+    const match = squareIdString.match(regex);
+  
+    if (match) {
+      const rowNumber = parseInt(match[1], 10); // Extract and parse the first number
+      const columnNumber = parseInt(match[2], 10); // Extract and parse the second number
+      return { rowNumber, columnNumber };
+    } else {
+      return null; // Return null if the string doesn't match the pattern
+    }
+  }
+  
+  function _isWithinGrid(number, gridSize) {
+    return number > 0 && number <= gridSize;
+  }
+  
+  function _reduceNumber(number, gridSize) {
+    if (_isWithinGrid(number-1, gridSize)) {
+      return number - 1;
+    }
+    return number;
+  }
+  
+  function _increaseNumber(number, gridSize) {
+    if (_isWithinGrid(number+1, gridSize)) {
+      return number + 1;
+    }
+    return number;
+  }
+  
+  function _constructScope(gridData) {
+    const { rowNumber, columnNumber, gridSize } = gridData; 
+    _hoverScope.clear();
+  
+    // checking left and right
+      _hoverScope.add(`sq-${rowNumber}-${_reduceNumber(columnNumber, gridSize)}`);
+      _hoverScope.add(`sq-${rowNumber}-${_increaseNumber(columnNumber, gridSize)}`);
+  
+    // checking top left, bottom left
+      _hoverScope.add(`sq-${_reduceNumber(rowNumber, gridSize)}-${_reduceNumber(columnNumber, gridSize)}`);
+      _hoverScope.add(`sq-${_increaseNumber(rowNumber, gridSize)}-${_reduceNumber(columnNumber, gridSize)}`);
+  
+    // checking top, bottom
+      _hoverScope.add(`sq-${_reduceNumber(rowNumber, gridSize)}-${columnNumber}`);
+      _hoverScope.add(`sq-${_increaseNumber(rowNumber, gridSize)}-${columnNumber}`);
+  
+    // checking top right, bottom right
+      _hoverScope.add(`sq-${_reduceNumber(rowNumber, gridSize)}-${_increaseNumber(columnNumber, gridSize)}`);
+      _hoverScope.add(`sq-${_increaseNumber(rowNumber, gridSize)}-${_increaseNumber(columnNumber, gridSize)}`);
+  }
+  
+  function _getSurroundingScrope(squareID) {
+    const gridSize = appData.gridSize;
+    const { rowNumber, columnNumber } = extractNumbers(squareID);
+    _constructScope({ rowNumber, columnNumber, gridSize });
+  } 
+  
+  function _isWithinHoverScope(squareID) {
+    return _hoverScope.has(squareID);
+  }
+
+  return {
+    _getSurroundingScrope,
+    _isWithinHoverScope,
+  }
 }
