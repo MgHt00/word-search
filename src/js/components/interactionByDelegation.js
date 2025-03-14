e9a07c690bd791d167e04be7ce14c16f5ce8d2bb
Mg Htoo <whshwe@gmail.com>
2025-03-14 15:39:26 +0630

    logic goes well, need complicated funtion to get data from Global

--- src/js/components/interactionByDelegation.js
+++ src/js/components/interactionByDelegation.js
@@ -1,15 +1,108 @@
 export function interactionManager(globals) {
   const { selectors, wordPlacementData } = globals;
   const { squareFrame } = selectors;
-  const { startIDAndEndID } = wordPlacementData;
+  const { placedWordCoordinates, startIDAndEndID } = wordPlacementData;
+
+  const _interactionState = {
+    _targetEndSquare: null,
+    _wordMatchTimeout: null,
+
+    setTargetEndSquare(value) {
+      this._targetEndSquare = value;
+    },
+
+    getTargetEndSquare() {
+      return this._targetEndSquare;
+    },
+
+    setWordMatchTimeout(timeout) {
+      this._endPointTimeout = timeout;
+    },
+
+    getWordMatchTimeout() {
+      return this._endPointTimeout;
+    },
+  }
 
   function addClickListener() {
     squareFrame.addEventListener("click", (event) => {
       if (event.target.matches('[class|="sq"]')) {
         const clickedSquare = event.target.id;
-        console.log("Clicked square ID:", clickedSquare);
-        // Add your game logic here...
+
+        if (_isStartSquare(clickedSquare)) {
+          _handleStartSquareClick(clickedSquare);
+        } 
+                
+        else {
+          _handleOtherSquareClick(clickedSquare);
+        }        
+
       }
+    });
+  }
+
+  function _isStartSquare(squareID) {
+    return startIDAndEndID.has(squareID);
+  }
+
+  function _getEndSquareFromMap(key) {
+    return startIDAndEndID.get(key);
+  }
+
+  function _getSquareIDsFromGlobal(key) {
+    
+  }
+
+  function _findSelectedWordInGlobal(endSquareID) {
+
+  }
+
+  function _handleStartSquareClick(squareID) {
+    const _targetEndSquare = _getEndSquareFromMap(squareID);
+    _interactionState.setTargetEndSquare(_targetEndSquare);
+    console.info("_targetEndSquare", _interactionState.getTargetEndSquare());
+    
+    console.info("placedWordCoordinates:", placedWordCoordinates);
+
+    // Set a timeout to reset _wordMatchTimeout
+    const timeoutId = setTimeout(() => {
+      _resetAllSquares();
+      console.warn("Timeout: Word Match reset.");
+    }, 10000);
+
+    _interactionState.setWordMatchTimeout(timeoutId);
+  }
+
+  function _handleOtherSquareClick(squareID) {
+    console.info("Other square clicked:",squareID);
+
+    if (squareID === _interactionState.getTargetEndSquare()) {
+      console.warn("BINGOOOOO!!!!");
+      _resetAllSquares();
+      const squaresToFill = _placedWordData.getWordDetails(selectedWord, "squareIDs");
+      _highlightCompletedWord(squaresToFill);
+    }
+  }
+
+  function _resetAllSquares() {
+    clearTimeout(_interactionState.getWordMatchTimeout());
+    //clearTimeout(_interactionState.getHoverTimeout());
+
+    const allSquares = document.querySelectorAll('[class|="sq"]');
+    allSquares.forEach((squareDOMElement) => {
+      squareDOMElement.classList.remove("tracer");
+    });
+
+    //_interactionState.setTracerFlag(false);
+    _interactionState.setTargetEndSquare(null);
+  }
+
+  // Fill the squares with the highlight class.
+  function _highlightCompletedWord(squares) {
+    squares.forEach((square) => {
+      const squareID = `#${square}`;
+      document.querySelector(squareID).classList.remove("clicked");
+      document.querySelector(squareID).classList.add("highlight");
     });
   }
 
