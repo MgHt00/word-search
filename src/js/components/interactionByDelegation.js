7f7d7e3fdd642319c4e8e8259559d7bfcf7ec549
Mg Htoo <whshwe@gmail.com>
2025-03-19 10:49:57 +0630

    all EndSquare functions in pural forms

--- src/js/components/interactionByDelegation.js
+++ src/js/components/interactionByDelegation.js
@@ -4,25 +4,34 @@ export function interactionManager( globals, isStartSquare, getEndSquaresFromGlo
   const { placedWordCoordinates } = wordPlacementData;
 
   const _interactionState = {
-    _targetEndSquare: null,
-      setTargetEndSquare(value) { this._targetEndSquare = value; },
-      getTargetEndSquare() { return this._targetEndSquare; },
-
     _targetEndSquares: [],
       addTargetEndSquares(squares) {
         squares.forEach(square => {
           this._targetEndSquares.push(square);
         });
+        console.info("addTargetEndSquares:", this._targetEndSquares);
       },
+
       getTargetEndSquares() { return this._targetEndSquares; },
-      hasTargetEndSquare(squareID) {
-        return this._targetEndSquares.includes(squareID);
+
+      hasTargetEndSquares(squareIDs) {
+        squareIDs = Array.isArray(squareIDs) ? squareIDs : [squareIDs];
+        return squareIDs.some(squareID => this._targetEndSquares.includes(squareID));
       },
-      removeTargetEndSquare(squareID) {
-        const index = this._targetEndSquares.indexOf(squareID);
-        if (index !== -1) {
-          this._targetEndSquares.splice(index, 1);
-        }
+
+      removeTargetEndSquares(squareIDs) {
+        squareIDs = Array.isArray(squareIDs) ? squareIDs : [squareIDs];
+        squareIDs.forEach(squareID => {
+          const index = this._targetEndSquares.indexOf(squareID);
+          if (index !== -1) {
+            this._targetEndSquares.splice(index, 1);
+          }
+        });
+        console.info("removeTargetEndSquares:", this._targetEndSquares);
+      },
+      
+      resetTargetEndSquares(){
+        this._targetEndSquares = [];
       },
 
     _wordMatchTimeout: null,
@@ -47,13 +56,13 @@ export function interactionManager( globals, isStartSquare, getEndSquaresFromGlo
 
   function _addSquareListeners() {
     squareFrame.addEventListener("click", (event) => {
-      if (event.target.matches('[id|="sq"]') && !event.target.classList.contains("disabled")) {
+      if (event.target.matches('[id|="sq"]')) {
         _handleClick(event.target);
       }
     });
 
     squareFrame.addEventListener("mouseover", (event) => {
-      if (event.target.matches('[id|="sq"]') && !event.target.classList.contains("disabled")) {
+      if (event.target.matches('[id|="sq"]')) {
         _handleHover(event.target);
       }
     });
@@ -75,32 +84,35 @@ export function interactionManager( globals, isStartSquare, getEndSquaresFromGlo
 
   function _handleStartSquareClick(squareID) {
     const _targetEndSquares = getEndSquaresFromGlobal(squareID);
-    _interactionState.addTargetEndSquares(_targetEndSquares);
-    
-    // Set a timeout to reset _wordMatchTimeout
-    const timeoutId = setTimeout(() => {
-      _resetAllSquares();
-    }, 5000);
+    if (_targetEndSquares) {
+      _interactionState.addTargetEndSquares(_targetEndSquares);
 
-    _interactionState.setWordMatchTimeout(timeoutId);
+      // Set a timeout to reset _wordMatchTimeout
+      const timeoutId = setTimeout(() => {
+        _resetAllSquares();
+      }, 5000);
+
+      _interactionState.setWordMatchTimeout(timeoutId);
+    }
   }
 
   function _handleOtherSquareClick(squareID) {
     console.info("_handleOtherSquareClick:",squareID);
 
     // if end square is clicked
-      if (_interactionState.hasTargetEndSquare(squareID)) {
-      console.warn("BINGOOOOO!!!!");
+      if (_interactionState.hasTargetEndSquares(squareID)) {
+      console.info("we are in.")
       const selectedWord = findSelectedWordInGlobal(squareID);
       if (selectedWord) {
+        console.warn("BINGOOOOO!!!!");
         const squaresToFill = getSquareIDsOfSelectedWord(selectedWord);
         _highlightCompletedWord(squaresToFill);
         _markCompletedWord(selectedWord.toLowerCase());
         _handleGameCompletion();
-      }
 
-      _interactionState.removeTargetEndSquare(squareID);
-      _resetAllSquares();
+        _interactionState.removeTargetEndSquares(squareID);
+        _resetAllSquares();
+      }
     }
   }
 
@@ -137,7 +149,7 @@ export function interactionManager( globals, isStartSquare, getEndSquaresFromGlo
     });
 
     _interactionState.setTracerFlag(false);
-    _interactionState.setTargetEndSquare(null);
+    _interactionState.resetTargetEndSquares();
   }
 
   function _clearAllTimeOuts() {
