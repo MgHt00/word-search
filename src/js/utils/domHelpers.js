66ec305d50787973be9f9112668bfc7f58de2d6b
Mg Htoo <whshwe@gmail.com>
2025-03-21 12:27:52 +0630

    update DOMHelper

--- src/js/utils/domHelpers.js
+++ src/js/utils/domHelpers.js
@@ -1,8 +1,21 @@
-export function createUL(className = "") {
+/*export function createUL(className = "") {
   let ulElement = document.createElement("ul");
   ulElement.id = "word-list";
   if (className) {
     ulElement.classList.add(className);
   }
+  return ulElement;
+}*/
+
+export function createUL(className) { // using bootstrap
+  const ulElement = document.createElement("ul");
+  ulElement.id = "word-list"; // Add an ID for easier selection later
+
+  // Add Bootstrap classes for styling
+  ulElement.classList.add("list-group"); // Basic list styling
+  if (className) {
+    ulElement.classList.add(className); // Add any additional custom classes
+  }
+
   return ulElement;
 }
